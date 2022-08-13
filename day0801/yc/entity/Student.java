956b710b42052b219cfbda4bd349827ff66c0ce3
chunyang20 <111059667+chunyang20@users.noreply.github.com>
2022-08-13 09:55:44 +0800

    Add files via upload

---
+++ day0801/yc/entity/Student.java
@@ -0,0 +1,107 @@
+package cn.day0801.yc.entity;
+
+public class Student {
+    private int id;
+    private String name;
+    private boolean sex;
+    private int age;
+    private String phone;
+    private String classes;
+    private String address;
+    /* 状态  0在读  1毕业  -1休学*/
+    private int state;
+
+    public Student(int id, String name, boolean sex, int age, String phone, String classes, String address, int state) {
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.phone = phone;
+        this.classes = classes;
+        this.address = address;
+        this.state = state;
+    }
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sex=" + sex +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                ", classes='" + classes + '\'' +
+                ", address='" + address + '\'' +
+                ", state=" + state +
+                '}';
+    }
+
+
+}
